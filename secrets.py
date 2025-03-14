6c4aefa9b88a2b628daee913d059d12b8d309193
Alexandr Semionov <122827524+Alexandralexv@users.noreply.github.com>
2025-03-14 08:15:40 +0000

    C_ost = 2000000
    C_ost = 2000000

---
+++ secrets.py
@@ -0,0 +1,9 @@
+import os
+
+secret1 = os.environ['SECRET1']
+secret2 = os.environ['SECRET2']
+secret3 = os.environ['SECRET3']
+
+print('secret1: ', secret1)
+print('secret2: ', secret2)
+print('secret3: ', secret3)
---
+++ semionov.sh
@@ -0,0 +1,4 @@
+echo Это первый скрипт
+touch testfile.txt
+ls
+echo Конец скрипта
